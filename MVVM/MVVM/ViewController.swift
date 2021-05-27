5c62a3c3d510e2dddb4c95455f7461acb9a5bd98
AlexZhadaev <pvt.vatnik@gmail.com>
2021-05-27 22:39:48 +0300

    Project created

---
+++ MVVM/MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Жадаев Алексей on 27.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
