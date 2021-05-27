0be7e7cef5756eae6ce739845b94703cbdafa9a2
AlexZhadaev <pvt.vatnik@gmail.com>
2021-05-27 23:44:30 +0300

    upd.

---
+++ MVVM/MVVM/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM
+//
+//  Created by Жадаев Алексей on 27.05.2021.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
